5fd354d038f1def50157801dcfd78cc7df3e2650
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-08-21 22:32:24 -0400

    commit tree b1989b118d1cec94d3d9d065dc2c1cf586363174

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    return n.toString();
+}
+
+function makeInt(n, base = 10) {
+    return parseInt(n, base)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
